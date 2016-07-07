f45b2527e9401a52c9814aa19580d3f42a0a051e
Joseph Severino <joseph.severino@colorado.edu>
2016-07-07 18:31:42 +0000

    still working on Victims and Volunteers

--- victims-and-volunteers/victimsVol.js
+++ victims-and-volunteers/victimsVol.js
@@ -1,14 +1,28 @@
 var userInput = prompt("How many disaster victims do you have");
 
-var names = {};
-var phoneNumbers = {};
-var street = {};
+var names = [];
+var phoneNumbers = [];
+var street = [];
 
 for(var i=0; i < userInput; i++){
-    names[i]=prompt("What's the name?");
+    names.push(prompt("What's the name?"));
   
    
 } 
+
+for(var i=0; i < userInput; i++){
+    phoneNumbers.push(prompt("What's the phone number of " + names[i] +"?"));
+  
+   
+} 
+
+for(var i=0; i < userInput; i++){
+    street.push(prompt("What's the street for " + names[i] +"?"));
+  
+   
+} 
+alert( street.join(', ') + "\n" + names.join(', ') + "\n" + street.join(','))
+
 
 
 
